49c3f5c7d2ab943e557cd5190e8e7f98d27e4dd0
harzyz <110671828+harzyz@users.noreply.github.com>
2023-11-12 18:49:45 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let caret = document.getElementById('arrow')
+let caret1 = document.getElementById('arrow1')
+let foo = document.getElementById('blogList')
+let sidenav = document.getElementById('navShow')
+let image = './images/Vector (9).png'
+let toggle = false
+let beans = false
+
+function toggleCaret(button){
+  toggle = !toggle
+  if(toggle){
+    foo.style.display = 'block'
+    button.style.transform = "rotate(180deg)"
+  }else{
+    foo.style.display = 'none'
+    button.style.transform = "rotate(0deg)"
+  }
+}
+
+function toggleNav(){
+  beans = !beans
+  if(beans){
+    sidenav.style.display = 'block'
+  }else{
+    sidenav.style.display = 'none'
+  }
+}
