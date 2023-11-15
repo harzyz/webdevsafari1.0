18927f02416145143659427eedbb084be7fae5cd
harzyz <110671828+harzyz@users.noreply.github.com>
2023-11-15 17:19:07 +0100

    first commit

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 let caret = document.getElementById('arrow')
 let caret1 = document.getElementById('arrow1')
-let foo = document.getElementById('blogList')
 let sidenav = document.getElementById('navShow')
 let image = './images/Vector (9).png'
 let toggle = false
 let beans = false
 
-function toggleCaret(button){
+let foo = document.getElementById('blogList')
+function toggleCaret(){
+  foo.classList.toggle('show')
   toggle = !toggle
+  let button = document.getElementById('arrow1');
   if(toggle){
-    foo.style.display = 'block'
     button.style.transform = "rotate(180deg)"
   }else{
-    foo.style.display = 'none'
     button.style.transform = "rotate(0deg)"
   }
 }
+let boo = document.getElementById('courseList')
+function toggleCaret2(){
+  boo.classList.toggle('show')
+  toggle = !toggle
+  let button = document.getElementById('arrow');
+  if(toggle){
+    button.style.transform = "rotate(180deg)"
+  }else{
+    button.style.transform = "rotate(0deg)"
+  }
+}
+
 
 let subMenu = document.getElementById('navShow')
 
